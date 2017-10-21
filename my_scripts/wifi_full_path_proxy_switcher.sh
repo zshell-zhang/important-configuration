5e57fa19c707de31c6271cbfca275956feb32147
zshell.zhang <zshell.zhang@qunar.com>
2017-10-21 19:28:20 +0800

    add sudo to wifi proxy switcher

--- my_scripts/wifi_full_path_proxy_switcher.sh
+++ my_scripts/wifi_full_path_proxy_switcher.sh
@@ -3,16 +3,16 @@
 command=$1
 
 if [[ ${command}x = 'onx' ]]; then
-    iptables -t nat -A PREROUTING -d 127.0.0.0/24 -j RETURN
-    iptables -t nat -A PREROUTING -d 192.168.0.0/16 -j RETURN
-    iptables -t nat -A PREROUTING -d 10.42.0.0/16 -j RETURN
-    iptables -t nat -A PREROUTING -p tcp -s 10.42.0.0/16 -j REDIRECT --to-ports 12345
+    sudo iptables -t nat -A PREROUTING -d 127.0.0.0/24 -j RETURN
+    sudo iptables -t nat -A PREROUTING -d 192.168.0.0/16 -j RETURN
+    sudo iptables -t nat -A PREROUTING -d 10.42.0.0/16 -j RETURN
+    sudo iptables -t nat -A PREROUTING -p tcp -s 10.42.0.0/16 -j REDIRECT --to-ports 12345
     echo 'laptop wifi use redsocks redirecting packages to shadowsocks-client!'
 elif [[ ${command}x = 'offx' ]]; then
-    iptables -t nat -D PREROUTING -d 127.0.0.0/24 -j RETURN
-    iptables -t nat -D PREROUTING -d 192.168.0.0/16 -j RETURN
-    iptables -t nat -D PREROUTING -d 10.42.0.0/16 -j RETURN
-    iptables -t nat -D PREROUTING -p tcp -s 10.42.0.0/16 -j REDIRECT --to-ports 12345
+    sudo iptables -t nat -D PREROUTING -d 127.0.0.0/24 -j RETURN
+    sudo iptables -t nat -D PREROUTING -d 192.168.0.0/16 -j RETURN
+    sudo iptables -t nat -D PREROUTING -d 10.42.0.0/16 -j RETURN
+    sudo iptables -t nat -D PREROUTING -p tcp -s 10.42.0.0/16 -j REDIRECT --to-ports 12345
     echo 'laptop wifi stop redirecting packages to shadowsocks-client!'
 fi
 
