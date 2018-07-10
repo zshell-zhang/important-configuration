57c9a4ed16f51e190eff0452e1f9ef1b228ba5e1
zshell.zhang <zshell.zhang@qunar.com>
2018-07-10 09:02:53 +0800

    update corporation ssh info

--- my_scripts/fast_login_ssh.sh
+++ my_scripts/fast_login_ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # qunar ssh remote server alias
 
-function sb() {
+function ssh_zshell() {
   if [[ -z "$1" ]]; then
 	echo "sb 1"
 	echo "e10445d33c"  #zhi.zhang
@@ -21,7 +21,27 @@ function sb() {
   fi
 }
 
-function st() {
+function sshb() {
+  if [[ -z "$1" ]]; then
+        echo "hongyu 1"
+        echo "e813927176"
+        ssh -p 22 hongyu.shao@10.64.0.11
+  elif [[ "$1"x = "1"x ]]; then
+        echo "hongyu 1"
+        echo "e813927176"
+        ssh -p 22 hongyu.shao@10.64.0.11
+  elif [[ "$1"x = "2"x ]]; then
+        echo "hongyu 2"
+        echo "e813927176"
+        ssh -p 22 hongyu.shao@10.64.0.12
+  else
+        echo "hongyu 1"
+        echo "e813927176"
+        ssh -p 22 hongyu.shao@10.64.0.11
+  fi
+}
+
+function sshq() {
   if [[ -n "$1" ]] ;then
       echo "zshell.zhang@l-rtools1.ops.cn$1.qunar.com"
       ssh zshell.zhang@l-rtools1.ops.cn$1.qunar.com
